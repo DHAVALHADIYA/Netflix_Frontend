a14fb56f2caa01a63d6c34f4c7c85e650da29992
DHAVALHADIYA <hadiyadhaval5@gmail.com>
2023-09-21 12:38:40 +0530

    added backend url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://netflix-clone-z1iq.onrender.com",
 });
 
 const instance = axios.create({
